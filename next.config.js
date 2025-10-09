ce538cea1bde1ca939ff9a3774bc0accea502184
sruja <113293177+srxja@users.noreply.github.com>
2025-10-09 12:31:37 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // domains: ['res.cloudinary.com'], // Uncomment and add your Cloudinary domain later
+  },
+};
+
+module.exports = nextConfig;
